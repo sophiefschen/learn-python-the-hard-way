0f24efadc19f724667982955913c558a54f7731a
sophiefschen <sophiefschen@sina.com>
2016-12-29 23:23:15 +0800

    Add files via upload
    ex11,ex12,ex13,ex14,ex15

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, a, second, third = argv
+x = raw_input("What is your name ?")
+
+print "The script is called:", script
+print "Your first variable is:", a
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "My name is",x
+
